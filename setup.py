6662f9ed5dc5b317fb4a7b87f02b3ab504d61c16
bouldinnathan <45728244+bouldinnathan@users.noreply.github.com>
2018-12-09 03:24:48 -0600

    setting up cmake for opencv

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if not(user_input2=='y'):
     exit()
 #--------------update all----------------------
 if user_input=='y':
-    print("starting update")
+    print("-------------starting update-----------------")
     output=os.system('sudo apt-get update -y')
     
     if output==0:
@@ -27,8 +27,8 @@ if user_input=='y':
     else :
         print('failed update')
 #--------------update all----------------------
-
-print('Getting dependencies…')
+##--------------dependencies------------------
+print('-------------Getting dependencies…---------------')
 output=os.system('sudo apt-get install build-essential cmake pkg-config -y')
 output=os.system('sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y')+output
 output=os.system('sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y')+output
@@ -40,6 +40,36 @@ if output==0:
     print('success dependencies')
 else :
     print('failed dependencies')
+##--------------dependencies------------------
+
+###-------------get Opencv------------------
+print('-------------Getting opencv--------------')
+output=os.system('wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip')
+output=os.system('unzip opencv.zip')+output
+output=os.system('sudo rm opencv.zip')+output
+output=os.system('wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip')+output
+output=os.system('unzip opencv_contrib.zip')+output
+output=os.system('sudo rm opencv_contrib.zip')+output
+if output==0:
+    print('success download of opencv')
+else :
+    print('failed download of opencv')
+
+#output=os.system('wget https://bootstrap.pypa.io/get-pip.py')
+#output=os.system('sudo python get-pip.py')
+
+output=os.system('pip3 install numpy')
+output=os.system('mkdir /opencv-3.1.0/build')+output
+output=os.system('cmake /opencv-3.1.0/build -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.1.0/modules \
+    -D BUILD_EXAMPLES=ON ..')+output
+if output==0:
+    print('success pre-install of opencv')
+else :
+    print('failed pre-install of opencv')
+
 
 
 
