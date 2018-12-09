f933d61bb786b5bbc8d635e2e04e2cb09fe8ab1f
bouldinnathan <45728244+bouldinnathan@users.noreply.github.com>
2018-12-09 07:02:01 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -55,10 +55,10 @@ else :
 ###-------------get Opencv------------------
 if user_input3!='n':
     print('-------------Getting opencv--------------')
-    output=os.system('wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.0.0.zip')
+    output=os.system('wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip')
     output=os.system('unzip opencv.zip')+output
     output=os.system('sudo rm opencv.zip')+output
-    output=os.system('wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.0.0.zip')+output
+    output=os.system('wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.3.0.zip')+output
     output=os.system('unzip opencv_contrib.zip')+output
     output=os.system('sudo rm opencv_contrib.zip')+output
     if output==0:
@@ -70,12 +70,12 @@ if user_input3!='n':
     #output=os.system('sudo python get-pip.py')
 
     output=os.system('pip3 install numpy')
-output=os.system('cd opencv-3.0.0; mkdir build;cd build')+output
+output=os.system('cd opencv-3.3.0; mkdir build;cd build')+output
 output1=os.system('cd opencv-3.0.0/build/;cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D ENABLE_PRECOMPILED_HEADERS=OFF \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.0.0/modules \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \
     -D BUILD_EXAMPLES=ON ..')
 
 if output==0:
@@ -89,7 +89,7 @@ else :
     print('Errors but should be fine...')
 
 
-output=os.system('cd opencv-3.0.0/build/;make')
+output=os.system('cd opencv-3.3.0/build/;make')
 if output==0:
     print('success make of opencv')
 else :
