bf445cf8c8202b3497f3da91c845b68716b9c694
bouldinnathan <45728244+bouldinnathan@users.noreply.github.com>
2018-12-09 02:45:03 -0600

    added the needed dependencies for opencv

--- setup.py
+++ setup.py
@@ -1,16 +1,56 @@
 import os
 
+print("Answer all of the questions. The setup will run unattended after that(~2hrs).")
+user_input=input('Would you like to update all (y/n)?')
+user_input2=input('Is the SD card at least 16GB (y/n)?')
+if not(user_input2=='y'):
+    print('The opencv library is a very large source and must be compiled. Exiting...')
+    exit()
+#--------------update all----------------------
+if user_input=='y':
+    print("starting update")
+    output=os.system('sudo apt-get update -y')
+    
+    if output==0:
+        print('success update')
+    else :
+        print('failed update')
+        
+    output=os.system('sudo apt-get upgrade -y')
+    if output==0:
+        print('success update')
+    else :
+        print('failed update')
+    output=os.system('sudo apt-get dist-upgrade -y')
+    if output==0:
+        print('success update')
+    else :
+        print('failed update')
+#--------------update all----------------------
 
-print("starting update")
-output=os.system('sudo apt-get update -y')
+print('Getting dependencies…')
+output=os.system('sudo apt-get install build-essential cmake pkg-config')
+output=os.system('sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev')+output
+output=os.system('sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev')+output
+output=os.system('sudo apt-get install libxvidcore-dev libx264-dev')+output
+output=os.system('sudo apt-get install libgtk2.0-dev')+output
+output=os.system('sudo apt-get install libatlas-base-dev gfortran')+output
+output=os.system('sudo apt-get install python2.7-dev python3-dev')+output
 if output==0:
-    print('good update')
+    print('success dependencies')
 else :
-    print('failed update')
-    
-output=os.system('sudo apt-get upgrade -y')
-if output==0:
-    print('good update')
-else :
-    print('failed update')
-    
+    print('failed dependencies')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
